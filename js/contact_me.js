2b9d2df4db3517c273018e3fc0890a3c4e4c166d
SimonM <podvilla@gmail.com>
2016-08-19 00:10:52 +0100

    Completed jekyll version of site

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,34 +1,37 @@
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+            
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+            var payLoad = { "text": "A message from the bitshifters website from: Name: '" + name + "', Email: '" + email + "', Message: '" + message + "'." };
+
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://hooks.slack.com/services/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",
                 type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+                data: 'payload=' + JSON.stringify({
+                    "text": "A message from the bitshifters website from: Name: '" + name + "', Email: '" + email + "', Message: '" + message + "'."
+                }),
+                dataType: 'json',
                 cache: false,
                 success: function() {
-                    // Success message
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
@@ -40,7 +43,24 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function () {
+
+                    // sm: knackered response from slack. Lets Just assume it works.
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+
+                    /*
+
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -49,8 +69,9 @@ $(function() {
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
+                    */
                 },
-            })
+            });
         },
         filter: function() {
             return $(this).is(":visible");
@@ -63,8 +84,7 @@ $(function() {
     });
 });
 
-
-/*When clicking on Full hide fail/success boxes */
+// When clicking on Full hide fail/success boxes
 $('#name').focus(function() {
     $('#success').html('');
 });
