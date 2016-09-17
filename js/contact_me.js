9d6ccf2431c34b2cdc97bab7ba2f77a852a4df6b
SimonM <podvilla@gmail.com>
2016-09-17 14:02:44 +0100

    Updated site to support submissions & contacts

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    // contact form
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -19,8 +20,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            var payLoad = { "text": "A message from the bitshifters website from: Name: '" + name + "', Email: '" + email + "', Message: '" + message + "'." };
-
+ 
             $.ajax({
                 url: "https://hooks.slack.com/services/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",
                 type: "POST",
@@ -58,6 +58,86 @@ $(function() {
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
+
+                    /*
+
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                    */
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    // submission form
+    $("#submitForm input,#submitForm textarea,#submitForm select").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+            
+            // get values from FORM
+            var type = $("select#type").val();
+            var url = $("input#url").val();
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+  
+            $.ajax({
+                url: "https://hooks.slack.com/services/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",
+                type: "POST",
+                data: 'payload=' + JSON.stringify({ "channel" : "#compos",
+                    "text": "A submission from the bitshifters website of type: '" + type + "', url '" + url + "', Name: '" + name + "', Email: '" + email + "', Message: '" + message + "'."
+                }),
+                dataType: 'json',
+                cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function () {
+
+                    // sm: knackered response from slack. Lets Just assume it works.
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#submitForm').trigger("reset");
 
                     /*
 
